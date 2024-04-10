f659f07150ad6c5f441554fd4c8679eae272c5e6
aakash_rathor <154987650+aakashrathor2019@users.noreply.github.com>
2024-04-10 22:29:20 +0530

    Update diamond_shpape.py

--- diamond_shpape.py
+++ diamond_shpape.py
@@ -1,11 +1,13 @@
+#Print a diamond shape with 2n rows
 def print_diamond(n):
-    # Upper half of the diamond
+    # Upper part of the diamond
     for i in range(1, n + 1):
         print(' ' * (n - i) + '*' * (2 * i - 1))
     
-    # Lower half of the diamond
+    # Lower part of the diamond
     for i in range(n - 1, 0, -1):
         print(' ' * (n - i) + '*' * (2 * i - 1))
 
-# Test the function with n = 4
-print_diamond(4)
+# Main program
+n = int(input("Enter the number of rows for the diamond: "))
+print_diamond(n)
